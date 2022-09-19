508670981646a51bc2ccff346382af934cd8cb6a
iammiracle <miracleaayodele@gmail.com>
2022-09-19 11:01:30 +0200

    show delivery vat based on fiscal position

---
+++ purchase_delivery_vat_report/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    'name': 'Purchase: Controls VAT on Purchase Report',
+    'version': '14.0.0.0.1',
+    'category': 'Purchase Management',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-purchase/purchase_delivery_vat_report',
+    'license': 'LGPL-3',
+    'summary': 'Controls VAT on Purchase Report',
+    'depends': [
+        'base_company_fiscal_position',
+        'purchase_stock'
+    ],
+    'data': [
+        'report/purchase_report_templates.xml',
+    ],
+    'installable': True,
+}
