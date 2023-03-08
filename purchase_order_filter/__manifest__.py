de5f0766ae73647b29fa81e4f40f255768e46cfd
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2023-03-08 13:15:08 +0100

    Update __manifest__.py
    minor spelling errors

--- purchase_order_filter/__manifest__.py
+++ purchase_order_filter/__manifest__.py
@@ -28,6 +28,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'summary': 'Purchase Order Filter',
+    'category': 'Inventory',
     'description': """
     Defaults purchase to my purchases.
     """,
